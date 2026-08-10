8ea54168fc012b09706afa6ae2a25654a8a43f59
JESHWANTH MANI <jeshwanthmanimani@gmail.com>
2026-08-10 18:13:09 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,11 @@
+public class Ifelse {
+  public static void main(String[] args) {
+    int age = 60;
+if(age >= 18){
+System.out.println("Allowed");
+  }
+else{
+System.out.println("Not allowed");
+}
+}
+}
