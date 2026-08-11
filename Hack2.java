ffb973f1df273aec916508c99980eb52d4397fda
JESHWANTH MANI <jeshwanthmanimani@gmail.com>
2026-08-11 19:40:34 +0530

    Add files via upload

---
+++ Hack2.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Hack2 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        String myString = scan.next();
+       int myInt = scan.nextInt();
+       scan.close();
+        System.out.println(a);
+        System.out.println(myString);
+        System.out.println(myInt);
+    }
+}
