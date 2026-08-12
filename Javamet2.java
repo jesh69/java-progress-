69e44707eca07cda4e52acda73f1cb9b2e6fbf01
JESHWANTH MANI <jeshwanthmanimani@gmail.com>
2026-08-12 19:45:37 +0530

    Add files via upload

---
+++ Javamet2.java
@@ -0,0 +1,9 @@
+public class Javamet2 {
+  static void welcomeMessage( String name) {
+    System.out.println(name);
+  }
+
+  public static void main(String[] args) {
+    welcomeMessage("jesh");
+  }
+}
