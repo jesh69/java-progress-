b0a0ff960725c39ac28e2fa62f07949b9195d487
JESHWANTH MANI <jeshwanthmanimani@gmail.com>
2026-08-12 19:55:38 +0530

    Add files via upload

---
+++ Scope.java
@@ -0,0 +1,10 @@
+public class Scope {
+  public static void main(String[] args) {
+
+    for (int i = 0; i < 5; i++) {
+      System.out.println(i); // i is accessible here
+    }
+
+    // i is NOT accessible here
+  }
+}
